bd46070dc0dc80db4122c6fc23f6c656ed0f9e28
Felipe Naia <fbruchn@gmail.com>
2018-12-05 14:51:23 -0200

    Add files via upload

--- GameKeyboardJS/src/main.js
+++ GameKeyboardJS/src/main.js
@@ -1,4 +1,12 @@
 let c = new Function("a", 'b',
 "let c = a*b;"+
 "let d = c*c;"+
-"return d")
+"return d");
+
+function a(){
+    log("a")
+}
+
+let jqtest = "addEventListener('click', a)";
+
+eval(jqtest)
